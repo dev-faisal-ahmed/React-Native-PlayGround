8ef2100d46646333580ba8cd2073d1cf10269697
dev-faisal-ahmed <ost.faisal.ahmed@gmail.com>
2024-03-12 01:45:31 +0600

    master branch cleaned

--- App.js
+++ App.js
@@ -1,39 +1,10 @@
-import { useState } from "react";
-import { Button, Modal, StyleSheet, View } from "react-native";
-import { AllGoals } from "./src/components/AllGoals";
-import { GoalInput } from "./src/components/GoalInput";
-import { colors } from "./src/styles-data/colors";
-import { StatusBar } from "expo-status-bar";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function App() {
-  const [goals, setGoals] = useState([]);
-  const [showModal, setShowModal] = useState(false);
-
-  const onGoalAdded = (goal) => {
-    setGoals((prev) => [...prev, goal]);
-  };
-
-  const onHideModal = () => {
-    setShowModal(false);
-  };
-
-  const onRemove = (goal) => {
-    const remainingGoals = goals.filter((eachGoal) => eachGoal !== goal);
-    setGoals(remainingGoals);
-  };
-
   return (
     <>
-      <StatusBar style="light" />
       <View style={styles.appContainer}>
-        {/* inputs */}
-        <Button title="Add New Goal" color={colors.primaryColor} onPress={() => setShowModal(true)} />
-
-        <Modal visible={showModal} animationType="slide">
-          <GoalInput onHideModal={onHideModal} onGoalAdded={onGoalAdded} />
-        </Modal>
-
-        <AllGoals goals={goals} onRemove={onRemove} />
+        <Text style={styles.text}>Hey There!!</Text>
       </View>
     </>
   );
@@ -41,10 +12,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    paddingTop: 60,
-    paddingHorizontal: 30,
     flex: 1,
-    gap: 10,
-    backgroundColor: colors.mainBackgroundColor,
+    justifyContent: "center",
   },
+  text: { textAlign: "center" },
 });
--- src/styles-data/colors.js
+++
@@ -1,8 +0,0 @@
-export const colors = {
-  primaryColor: "#5e0acc",
-  errorColor: "#f31282",
-  inputColor: "#e4d0ff",
-  textColor: "#120438",
-  backgroundColor: "#311b6b",
-  mainBackgroundColor: "#1e085a",
-};
